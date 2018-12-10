4f4a4dc8c69f38462e9e3881ca720386fdddfd2e
Arracias <shpakovnikita1998@gmail.com>
2018-12-10 22:13:37 +0300

    working appbar added

--- app/src/main/java/com/example/shaft/softwaredesign/MainActivity.java
+++ app/src/main/java/com/example/shaft/softwaredesign/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.shaft.softwaredesign;
 
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,14 @@ public final class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mActionBarToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
