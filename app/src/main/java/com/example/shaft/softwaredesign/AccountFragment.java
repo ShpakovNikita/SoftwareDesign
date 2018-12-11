f195ae1574caa502d0088cad97dc60847ca980d1
Arracias <shpakovnikita1998@gmail.com>
2018-12-11 19:39:43 +0300

    added base account page

---
+++ app/src/main/java/com/example/shaft/softwaredesign/AccountFragment.java
@@ -0,0 +1,19 @@
+package com.example.shaft.softwaredesign;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AccountFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
