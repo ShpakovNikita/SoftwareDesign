83501a53897c95d71d8296141fa40d3db5f432ec
n_shpakov2 <shpakovnikita1998@gmail.com>
2018-12-18 01:29:44 +0300

    working web view and loading animation added

--- app/src/main/java/com/example/shaft/softwaredesign/ui/WebActivity.java
+++ app/src/main/java/com/example/shaft/softwaredesign/ui/WebActivity.java
@@ -1,10 +1,15 @@
 package com.example.shaft.softwaredesign.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.shaft.softwaredesign.R;
 
@@ -23,6 +28,21 @@ public class WebActivity extends AppCompatActivity {
             WebView web_view = (WebView) findViewById(R.id.web_view);
             web_view.getSettings().setJavaScriptEnabled(true);
             web_view.setWebViewClient(new WebViewClient());
+
+            ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar);
+            TextView tv = (TextView) findViewById(R.id.loading);
+            CardView card = (CardView) findViewById(R.id.card_view);
+
+            web_view.setWebChromeClient(new WebChromeClient() {
+                public void onProgressChanged(WebView view, int progress) {
+                    bar.setProgress(progress);
+                    if(progress == 100) {
+                        bar.setVisibility(View.GONE);
+                        tv.setVisibility(View.GONE);
+                        card.setVisibility(View.GONE);
+                    }
+                }
+            });
             web_view.loadUrl(url);
         }
         else {
